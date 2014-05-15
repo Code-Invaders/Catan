f1cac56f6533205dd54e089e0c22ce9e9f6f4538
Matt Davis <matt.bronumski@btinternet.com>
2014-05-15 23:17:09 +0100

    Cleaned up namespaces

--- src/Catan.App/ResourceToColorConverter.cs
+++ src/Catan.App/ResourceToColorConverter.cs
@@ -2,7 +2,6 @@
 using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media;
-using Catan.App;
 
 namespace CodeInvaders.Catan.App
 {
--- src/Catan.App/TileFactory.cs
+++ src/Catan.App/TileFactory.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using Catan.App;
 
 namespace CodeInvaders.Catan.App
 {
